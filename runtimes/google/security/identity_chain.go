8303f180d0c7067129fcb331455874df427ff9e1
Asim Shankar <ashankar@google.com>
2014-06-05 00:42:33 -0700

    veyron2/security: Move the wire-format spec in veyron/runtime/google/security/wire to VDL in veyron2/security.
    Successfully operating with other veyron processes requires a common
    understanding of identities. Thus, the identity wire specification is deemed to
    be a "public API" rather than a runtime-specific implementation.
    
    Change-Id: I1501209ebf8e012a2b010bacb417db5ef5ece434

--- runtimes/google/security/identity_chain.go
+++ runtimes/google/security/identity_chain.go
@@ -13,10 +13,10 @@ import (
 
 	icaveat "veyron/runtimes/google/security/caveat"
 	"veyron/runtimes/google/security/keys"
-	"veyron/runtimes/google/security/wire"
 	"veyron/security/caveat"
 
 	"veyron2/security"
+	"veyron2/security/wire"
 	"veyron2/vom"
 )
 
