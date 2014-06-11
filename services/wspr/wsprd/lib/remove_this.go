892aac4ff310716349c6dcced2c2db89cc14f499
Robin Thellend <rthellend@google.com>
2014-06-11 11:09:27 -0700

    veyron/services/wspr/wsprd: Register vom types for rock-paper-scissors.
    Change-Id: Ib9d122715639f7aa0bca23d765e216b5719b642b

--- services/wspr/wsprd/lib/remove_this.go
+++ services/wspr/wsprd/lib/remove_this.go
@@ -1,12 +1,15 @@
 package lib
 
 import (
+	rps "veyron/examples/rockpaperscissors"
+	"veyron2/services/mounttable"
 	"veyron2/services/store"
 	"veyron2/services/watch"
 	"veyron2/vom"
 )
 
 func init() {
+	vom.Register(mounttable.MountEntry{})
 	vom.Register(store.Conflict{})
 	vom.Register(store.Entry{})
 	vom.Register(store.NestedResult(0))
@@ -16,4 +19,10 @@ func init() {
 	vom.Register(watch.Request{})
 	vom.Register(watch.ChangeBatch{})
 	vom.Register(watch.Change{})
+	vom.Register(rps.GameOptions{})
+	vom.Register(rps.GameID{})
+	vom.Register(rps.PlayResult{})
+	vom.Register(rps.PlayerAction{})
+	vom.Register(rps.JudgeAction{})
+
 }
--- services/wspr/wsprd/wspr.go
+++ services/wspr/wsprd/wspr.go
@@ -5,17 +5,12 @@ import (
 
 	"veyron/lib/signals"
 	"veyron/services/wspr/wsprd/lib"
-	"veyron2/services/mounttable"
-	"veyron2/vom"
 )
 
 func main() {
 	port := flag.Int("port", 8124, "Port to listen on")
 	veyronProxy := flag.String("vproxy", "", "The endpoint for the veyron proxy to publish on. This must be set")
 	flag.Parse()
-
-	// TODO(bprosnitz) Remove this once VOM2 goes in.
-	vom.Register(mounttable.MountEntry{})
 
 	proxy := lib.NewWSPR(*port, *veyronProxy)
 	defer proxy.Shutdown()
