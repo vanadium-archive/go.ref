930a64fd5028859b35e3f331b3cf4fc0968afddb
Asim Shankar <ashankar@google.com>
2014-07-23 12:30:16 -0700

    scripts/jenkins: Control script for running "integration" tests on jenkins.
    veyron-shell-test.sh finds all test.sh files in the source tree and
    executes them. The intention is to setup a jenkins configuration for
    this so that these "integration" and "binary" tests can be monitored.
    
    Change-Id: If901a69b671a53d967471a03f1b84f72c0b8fce8

--- examples/inspector/inspector/test.sh
+++ examples/inspector/inspector/test.sh
@@ -1,32 +1,34 @@
 #!/bin/sh
 
 tl=$(git rev-parse --show-toplevel)
-
 go=$tl/scripts/build/go
-
+bin=$tl/go/bin
 script=$0
+bd=$(dirname $script)
+bd=$(realpath $bd)
 
 cleanup() {
 	code=$1; msg=$2; shift; shift
 	[ "$*" != "" ] && echo "cleanup: $*"
-	rm -f .ep
+	rm -f $bd/.ep $bd/.id
 	kill $pid > /dev/null 2>&1
 	killall inspectord > /dev/null 2>&1
 	echo $script $msg
 	return $code
 }
 
-bd=$(dirname $0)
-echo "Build directory:" $bd
+($go install veyron/examples/inspector/... veyron/tools/identity/...) || cleanup 1 FAIL "binary builds failed" || exit 1
 
-(cd $bd; $go build .) || exit 1
-(cd $bd/../inspectord; $go build .) || exit 1
+# Generate an identity for the client and server
+# For now, using the same one for both
+$bin/identity generate "inspector" >$bd/.id || cleanup 1 FAIL "server identity" || exit 1
+export VEYRON_IDENTITY=$bd/.id
 
-(cd $bd/../inspectord; ./inspectord) 2> /dev/null > .ep &
+$bin/inspectord >$bd/.ep &
 pid=$!
 
 for i in 1 2 3 4; do
-	ep=$(cat .ep)
+	ep=$(cat $bd/.ep)
 	if [ "$ep" != "" ]; then
 		break
 	fi
@@ -35,7 +37,7 @@ done
 
 [ ! $ep ] && cleanup 0 FAIL "no server" && exit 1
 
-$bd/inspector --service /$ep/stubbed/files -glob='m*' || cleanup 1 FAIL "stubbed/files" || exit 1
-$bd/inspector --service /$ep/stubless/files -glob='m*'|| cleanup 1 FAIL "stubless/files" || exit 1
+$bin/inspector --service /$ep/stubbed/files -glob='m*' || cleanup 1 FAIL "stubbed/files" || exit 1
+$bin/inspector --service /$ep/stubless/files -glob='m*'|| cleanup 1 FAIL "stubless/files" || exit 1
 
 cleanup 0 PASS && exit 0
