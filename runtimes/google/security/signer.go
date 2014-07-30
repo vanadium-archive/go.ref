ad8634e4c48b44c3096612d38d6518f2c731e31c
Srdjan Petrovic <spetrovic@google.com>
2014-07-30 16:37:43 -0700

    veyron2/security:  Signature also stores the crypto hash function used to compute the signature.
    Motivation is that Java (as well as ssh-agent) hash the payload, so we need to hash again before verifying signatures.
    
    Change-Id: Ie53f9e0f91c78040985fb9ae5181935d727783b6

--- runtimes/google/security/signer.go
+++ runtimes/google/security/signer.go
@@ -9,6 +9,7 @@ import (
 
 // NewClearSigner creates a Signer that uses the provided private key to sign messages.
 // This private key is kept in the clear in the memory of the running process.
+// No hashing is applied prior to signing.
 func NewClearSigner(key *ecdsa.PrivateKey) security.Signer {
 	return &clearSigner{key}
 }
@@ -23,6 +24,7 @@ func (c *clearSigner) Sign(message []byte) (sig security.Signature, err error) {
 		return
 	}
 	sig.R, sig.S = r.Bytes(), s.Bytes()
+	sig.Hash = security.NoHash
 	return
 }
 
