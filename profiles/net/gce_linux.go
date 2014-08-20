57e63530d67211d2e1ba63194a4472eb4628d947
Cosmos Nicolaou <cnicolaou@google.com>
2014-08-19 21:45:25 -0700

    TBR: veyron/profiles/net: fix the build.
    Change-Id: Ib20e47081117a2b45d014a98c9956fa8e870b649

--- profiles/net/gce_linux.go
+++ profiles/net/gce_linux.go
@@ -6,6 +6,7 @@ import (
 	"net"
 
 	"veyron2"
+	"veyron2/config"
 
 	"veyron/profiles/internal/gce"
 )
@@ -14,15 +15,12 @@ func handleGCE(rt veyron2.Runtime, publisher *config.Publisher) bool {
 	log := rt.Logger()
 	if gce.RunningOnGCE() {
 		var pub net.IP
-		pub = publish_addr.IP
-		if pub == nil {
-			// Determine the IP address from GCE's metadata
-			if ip, err := gce.ExternalIPAddress(); err != nil {
-				log.Infof("failed to query GCE metadata: %s", err)
-			} else {
-				// 1:1 NAT case, our network config will not change.
-				pub = ip
-			}
+		// Determine the IP address from GCE's metadata
+		if ip, err := gce.ExternalIPAddress(); err != nil {
+			log.Infof("failed to query GCE metadata: %s", err)
+		} else {
+			// 1:1 NAT case, our network config will not change.
+			pub = ip
 		}
 		if pub == nil {
 			log.Infof("failed to determine public IP address to publish with")
@@ -31,9 +29,9 @@ func handleGCE(rt veyron2.Runtime, publisher *config.Publisher) bool {
 		ch := make(chan config.Setting)
 		defer close(ch)
 		if _, err := publisher.CreateStream(StreamName, "network configuration", ch); err != nil {
-			return nil, nil, err
+			return false
 		}
-		publishInitialSettings(ch, listenProtocol, listenSpec, prevAddr)
+		publishInitialSettings(ch, listen_protocol, listen_addr.IP.String(), pub)
 		return true
 	}
 	return false
