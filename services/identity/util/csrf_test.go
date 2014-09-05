a638996869e5a8b4eb227f4bedfcb66c500a8be9
Suharsh Sivakumar <suharshs@suharshs-macbookair.roam.corp.google.com>
2014-09-04 20:23:53 -0700

    veyron/services/identity/util: Fix broken build.
    Build broke due to unupdated test: csrf_test.go
    
    Change-Id: If8e9997a1076703095f7af30b909fbf1d8cab4a9

--- services/identity/util/csrf_test.go
+++ services/identity/util/csrf_test.go
@@ -8,48 +8,50 @@ import (
 )
 
 func TestCSRFTokenWithoutCookie(t *testing.T) {
+	cookieName := "VeyronCSRFTestCookie"
 	r := newRequest()
 	c := NewCSRFCop()
 	w := httptest.NewRecorder()
-	tok, err := c.NewToken(w, r)
+	tok, err := c.NewToken(w, r, cookieName)
 	if err != nil {
 		t.Errorf("NewToken failed: %v", err)
 	}
-	cookie := cookieSet(w)
+	cookie := cookieSet(w, cookieName)
 	if len(cookie) == 0 {
 		t.Errorf("Cookie should have been set. Request: [%v], Response: [%v]", r, w)
 	}
 	// Cookie needs to be present for validation
 	r.AddCookie(&http.Cookie{Name: cookieName, Value: cookie})
-	if err := c.ValidateToken(tok, r); err != nil {
+	if err := c.ValidateToken(tok, r, cookieName); err != nil {
 		t.Error("CSRF token failed validation:", err)
 	}
-	if err := c.ValidateToken(tok+"junk", r); err == nil {
+	if err := c.ValidateToken(tok+"junk", r, cookieName); err == nil {
 		t.Error("CSRF token should have failed validation")
 	}
 }
 
 func TestCSRFTokenWithCookie(t *testing.T) {
+	cookieName := "VeyronCSRFTestCookie"
 	r := newRequest()
 	c := NewCSRFCop()
 	w := httptest.NewRecorder()
 	r.AddCookie(&http.Cookie{Name: cookieName, Value: "u776AC7hf794pTtGVlO50w=="})
-	tok, err := c.NewToken(w, r)
+	tok, err := c.NewToken(w, r, cookieName)
 	if err != nil {
 		t.Errorf("NewToken failed: %v", err)
 	}
-	if len(cookieSet(w)) > 0 {
+	if len(cookieSet(w, cookieName)) > 0 {
 		t.Errorf("Cookie should not be set when it is already present. Request: [%v], Response: [%v]", r, w)
 	}
-	if err := c.ValidateToken(tok, r); err != nil {
+	if err := c.ValidateToken(tok, r, cookieName); err != nil {
 		t.Error("CSRF token failed validation:", err)
 	}
-	if err := c.ValidateToken(tok+"junk", r); err == nil {
+	if err := c.ValidateToken(tok+"junk", r, cookieName); err == nil {
 		t.Error("CSRF token should have failed validation")
 	}
 }
 
-func cookieSet(w *httptest.ResponseRecorder) string {
+func cookieSet(w *httptest.ResponseRecorder, cookieName string) string {
 	cookies := strings.Split(w.Header().Get("Set-Cookie"), ";")
 	for _, c := range cookies {
 		if strings.HasPrefix(c, cookieName) {
