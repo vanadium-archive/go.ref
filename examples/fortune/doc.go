8df069b882c39d22f6b60fae964efa262869200f
Jiri Simsa <jsimsa@google.com>
2014-09-10 17:03:16 -0700

    veyron/examples: moving examples into a separate repository
    Change-Id: I1600157d4b6c27929400f1df4457565ace245576

--- examples/fortune/doc.go
+++
@@ -1,3 +0,0 @@
-// Package fortune contains the most basic client/server application using the
-// Veyron API.  See http://go/veyron:code-lab for a thorough walk-through.
-package fortune
--- examples/pipetobrowser/browser/libs/utils/stream-helpers.js
+++
@@ -1,7 +0,0 @@
-import { default as es } from "npm:event-stream"
-
-export var streamUtil = {
-  split: es.split,
-  map: es.map,
-  writeArray: es.writeArray
-};
