2c4fdf1c9943096d7a11767ad077b5710b5c4a59
Bogdan Caprita <caprita@google.com>
2014-10-01 09:54:37 -0700

    veyron/services/mgmt/suidhelper/impl: set the default uid threshold back to 501 for darwin, now that Cos has updated the 'jenkins' uid on his Mac machine.
    Change-Id: Iec1265b97f0db0a502b16ef6133ec5465dd3a067

--- services/mgmt/suidhelper/impl/args_darwin.go
+++ services/mgmt/suidhelper/impl/args_darwin.go
@@ -1,6 +1,3 @@
 package impl
 
-// NOTE(caprita): The jenkins user id on Cos's Mac is 498.  We need to either
-// lower the threshold just for the tests to pass, or default the Mac threshold
-// to 498 or lower.  For now, do the latter.
-const uidThreshold = 498
+const uidThreshold = 501
