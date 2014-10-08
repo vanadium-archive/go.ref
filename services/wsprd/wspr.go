16c59e83f6a88c4f9d3f91a417102d463b02abf2
Jiri Simsa <jsimsa@google.com>
2014-10-08 10:39:25 -0700

    wspr: fixing a compilation error
    Change-Id: Ia41c8aa93da580c03ae6d88d9528193d73b6da80

--- services/wsprd/wspr.go
+++ services/wsprd/wspr.go
@@ -11,12 +11,13 @@ import (
 )
 
 func main() {
+	port := flag.Int("port", 8124, "Port to listen on.")
 	identd := flag.String("identd", "", "The endpoint for the identd server.  This must be set.")
 	flag.Parse()
 
 	rt.Init()
 
-	proxy := wspr.NewWSPR(*roaming.ListenSpec, *identd)
+	proxy := wspr.NewWSPR(*port, *roaming.ListenSpec, *identd)
 	defer proxy.Shutdown()
 	go func() {
 		proxy.Run()
