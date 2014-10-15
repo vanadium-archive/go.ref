35226a670dc663fdf5d869753f2c855fc544423e
Jiri Simsa <jsimsa@google.com>
2014-10-15 10:00:36 -0700

    examples: cleanup of shell tests for tunneld and inspectord
    Change-Id: I6ae7daf6459461a3be70f92f421fe4ea24c43cb7

--- tunneld/test.sh
+++ tunneld/test.sh
@@ -31,6 +31,10 @@ dumplogs() {
 }
 
 main() {
+  local EP
+  local GOT
+  local WANT
+
   cd "${WORKDIR}"
   build
   # Directory where all credentials will be stored (private key etc.)
@@ -40,65 +44,69 @@ main() {
   # Start mounttabled and find its endpoint.
   local -r MTLOG="${WORKDIR}/mt.log"
   touch "${MTLOG}"
-  ./mounttabled --veyron.tcp.address=127.0.0.1:0 > "${MTLOG}" 2>&1 &
-  shell::wait_for "${MTLOG}" "Mount table service at:"
-  local EP=$(grep "Mount table service at:" "${MTLOG}" | sed -e 's/^.*endpoint: //')
-  [[ -z "${EP}" ]] && shell_test::fail "line ${LINENO}: no mounttable server"
+
+  shell::run_server "${shell_test_DEFAULT_SERVER_TIMEOUT}" "${MTLOG}" "${MTLOG}" \
+    ./mounttabled --veyron.tcp.address=127.0.0.1:0 -vmodule=publisher=2 &> /dev/null \
+    || (dumplogs "${MTLOG}"; shell_test::fail "line ${LINENO}: failed to start mounttabled")
+  shell::timed_wait_for "${shell_test_DEFAULT_MESSAGE_TIMEOUT}" "${MTLOG}" "Mount table service at:" \
+    || (dumplogs "${MTLOG}"; shell_test::fail "line ${LINENO}: failed to start mount table service")
+  EP=$(grep "Mount table service at:" "${MTLOG}" | sed -e 's/^.*endpoint: //') \
+    || (dumplogs "${MTLOG}"; shell_test::fail "line ${LINENO}: failed to identify endpoint")
 
   # Start tunneld and find its endpoint.
   export NAMESPACE_ROOT="${EP}"
   local -r TUNLOG="${WORKDIR}/tunnel.log"
   touch "${TUNLOG}"
-  ./tunneld --address=127.0.0.1:0 -vmodule=publisher=2 > "${TUNLOG}" 2>&1 &
-  shell::wait_for "${TUNLOG}" "ipc pub: mount"
-  local EP=$(grep "Listening on endpoint" "${TUNLOG}" | sed -e 's/^.*endpoint //' | awk '{print $1}')
-  if [[ -z "${EP}" ]]; then
-    dumplogs "${TUNLOG}" "${MTLOG}"
-    shell_test::fail "line ${LINENO}: no tunnel server"
-  fi
+  shell::run_server "${shell_test_DEFAULT_SERVER_TIMEOUT}" "${TUNLOG}" "${TUNLOG}" \
+    ./tunneld --address=127.0.0.1:0 -vmodule=publisher=2 &> /dev/null \
+    || (dumplogs "${TUNLOG}"; shell_test::fail "line ${LINENO}: failed to start tunneld")
+  shell::timed_wait_for "${shell_test_DEFAULT_MESSAGE_TIMEOUT}" "${TUNLOG}" "ipc pub: mount" \
+    || (dumplogs "${TUNLOG}"; shell_test::fail "line ${LINENO}: failed to mount tunneld")
+  EP=$(grep "Listening on endpoint" "${TUNLOG}" | sed -e 's/^.*endpoint //' | awk '{print $1}') \
+    || (dumplogs "${TUNLOG}"; shell_test::fail "line ${LINENO}: failed to identify endpoint")
 
   # Run remote command with the endpoint.
   local -r VSHLOG="${WORKDIR}/vsh.log"
-  local GOT=$(./vsh --logtostderr --v=1 "/${EP}" echo HELLO ENDPOINT 2>"${VSHLOG}")
-  local WANT="HELLO ENDPOINT"
+  GOT=$(./vsh --logtostderr --v=1 "/${EP}" echo HELLO ENDPOINT 2>"${VSHLOG}") \
+    || (dumplogs "${VSHLOG}"; shell_test::fail "line ${LINENO}: failed to run vsh")
+  WANT="HELLO ENDPOINT"
 
   if [[ "${GOT}" != "${WANT}" ]]; then
-    dumplogs "${VSHLOG}" "${TUNLOG}" "${MTLOG}"
     shell_test::fail "line ${LINENO}: unexpected output. Got ${GOT}, want ${WANT}"
   fi
 
   # Run remote command with the object name.
-  GOT=$(./vsh --logtostderr --v=1 tunnel/hostname/$(hostname) echo HELLO NAME 2>"${VSHLOG}")
+  GOT=$(./vsh --logtostderr --v=1 tunnel/hostname/$(hostname) echo HELLO NAME 2>"${VSHLOG}") \
+    || (dumplogs "${VSHLOG}"; shell_test::fail "line ${LINENO}: failed to run vsh")
   WANT="HELLO NAME"
 
   if [[ "${GOT}" != "${WANT}" ]]; then
-    dumplogs "${VSHLOG}" "${TUNLOG}" "${MTLOG}"
     shell_test::fail "line ${LINENO}: unexpected output. Got ${GOT}, want ${WANT}"
   fi
 
   # Send input to remote command.
-  echo "HELLO SERVER" | ./vsh --logtostderr --v=1 "/${EP}" "cat > ${WORKDIR}/hello.txt" > "${VSHLOG}" 2>&1
+  echo "HELLO SERVER" | ./vsh --logtostderr --v=1 "/${EP}" "cat > ${WORKDIR}/hello.txt" 2>"${VSHLOG}" \
+    || (dumplogs "${VSHLOG}"; shell_test::fail "line ${LINENO}: failed to run vsh")
   GOT=$(cat "${WORKDIR}/hello.txt")
   WANT="HELLO SERVER"
 
   if [[ "${GOT}" != "${WANT}" ]]; then
-    dumplogs "${VSHLOG}" "${TUNLOG}" "${MTLOG}"
     shell_test::fail "line ${LINENO}: unexpected output. Got ${GOT}, want ${WANT}"
   fi
 
-  GOT=$(echo "ECHO" | ./vsh --logtostderr --v=1 "/${EP}" cat 2>"${VSHLOG}")
+  GOT=$(echo "ECHO" | ./vsh --logtostderr --v=1 "/${EP}" cat 2>"${VSHLOG}") \
+    || (dumplogs "${VSHLOG}"; shell_test::fail "line ${LINENO}: failed to run vsh")
   WANT="ECHO"
 
   if [[ "${GOT}" != "${WANT}" ]]; then
-    dumplogs "${VSHLOG}" "${TUNLOG}" "${MTLOG}"
     shell_test::fail "line ${LINENO}: unexpected output. Got ${GOT}, want ${WANT}"
   fi
 
   # Verify that all the published names are there.
-  GOT=$(./mounttable glob "${NAMESPACE_ROOT}" 'tunnel/*/*' |    \
-        sed -e 's/TTL .m..s/TTL XmXXs/'                     \
-            -e 's!hwaddr/[^ ]*!hwaddr/XX:XX:XX:XX:XX:XX!' | \
-        sort)
+  GOT=$(./mounttable glob "${NAMESPACE_ROOT}" 'tunnel/*/*' | \
+        sed -e 's/TTL .m..s/TTL XmXXs/' -e 's!hwaddr/[^ ]*!hwaddr/XX:XX:XX:XX:XX:XX!' | \
+        sort) \
+    || shell_test::fail "line ${LINENO}: failed to run mounttable"
   WANT="[${NAMESPACE_ROOT}]
 tunnel/hostname/$(hostname) /${EP}// (TTL XmXXs)
 tunnel/hwaddr/XX:XX:XX:XX:XX:XX /${EP}// (TTL XmXXs)"
