405360d8f69e62c4e9c9b3449ed536dd44e3da82
Jiri Simsa <jsimsa@google.com>
2014-11-10 16:31:28 -0800

    veyron: delete counterpart to go/vcl/6663
    Change-Id: Iccfd62f992b065a7cf84eac57b050aec7d873050

--- runtimes/google/lib/follow/bsd_darwin_linux_windows_config.go
+++
@@ -1,9 +0,0 @@
-// +build darwin freebsd linux netbsd openbsd windows
-
-package follow
-
-// newFSWatcher starts and returns a new fsnotify-based fsWatcher.
-// filename specifies the file to watch.
-func newFSWatcher(filename string) (fsWatcher, error) {
-	return newFSNotifyWatcher(filename)
-}
