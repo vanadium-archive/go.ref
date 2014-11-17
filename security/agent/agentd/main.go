8c8acea3d8ad7fdba2f61139c0ea3252de19d23e
Matt Rosencrantz <mattr@google.com>
2014-11-17 11:32:50 -0800

    veyron: Update go.tools to its new golang.org location.
    Change-Id: I65ea8a3f04c34271c17c903d10e7640fa3adf0be

--- security/agent/agentd/main.go
+++ security/agent/agentd/main.go
@@ -8,7 +8,7 @@ import (
 	"os/signal"
 	"syscall"
 
-	"code.google.com/p/go.crypto/ssh/terminal"
+	"golang.org/x/crypto/ssh/terminal"
 
 	"veyron.io/veyron/veyron/lib/flags/consts"
 	_ "veyron.io/veyron/veyron/profiles"
