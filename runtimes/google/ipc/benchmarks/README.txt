bc6ffa2911aed7e5c3cf2c99f0bbb0d49cf3d838
Jungho Ahn <jhahn@google.com>
2014-12-10 17:55:35 -0800

    Revise IPC benchmark and add a new benchmark on multiplexing.   o Changed IPC benchmark clients to add throughput measurement and     share it for benchmarks on multiplexing.   o Added a stand-alone benchmark binary bm/main.go for clean histogram     outputs. (With many benchmarks, it is very hard to read the     benchmark results when printing histogram in each benchmark     trials.)   o Added the latest benchmark results to RESULTS.txt
    Note:
      o There will be one follow-up CL to add a new benchmark to bmclient/bmserver.
      o I'm not sure when we got the BM results in README.txt, but a new BM results
        shows that the current IPC performance is 2 ~ 4X slower than before even
        with a better machine.
    
    Change-Id: I66942c33a849af2b37990d5368ede85cdb9b76cc

--- runtimes/google/ipc/benchmarks/README.txt
+++ runtimes/google/ipc/benchmarks/README.txt
@@ -1,6 +1,8 @@
 This directory contains code uses to measure the performance of the Veyron IPC
 stack.
 
+================================================================================
+
 The ipc_test.go file uses GO's testing package to run benchmarks. Each
 benchmark involves one server and one client. The server has two very simple
 methods that echo the data received from the client back to the client.
@@ -13,49 +15,63 @@ There are two versions of the Echo method:
  - EchoStream() <[]byte,[]byte> error
 
 The first benchmarks use the non-streaming version of Echo with a varying
-payload size. The other benchmarks use the streaming version with varying
-number of chunks and payload sizes.
+payload size. The second benchmarks use the streaming version with varying
+number of chunks and payload sizes. The third one is for measuring the
+performance with multiple clients hosted in the same process.
 
 All these benchmarks create a VC before measurements begin. So, the VC creation
 overhead is excluded.
 
-On a ThinkPad X1 Carbon (2 × Intel(R) Core(TM) i7-3667U CPU @ 2.00GHz), we get:
+$ veyron go test -test.bench=. -timeout=1h -test.cpu=1 -test.benchtime=5s \
+  veyron/runtimes/google/ipc/benchmarks
 
-$ veyron go test -test.bench=. -test.cpu=1 \
-	-test.benchtime=5s veyron/runtimes/google/ipc/benchmarks 2> /dev/null
-PASS
-Benchmark____1B	   10000	    545077 ns/op
-Benchmark___10B	   10000	    587312 ns/op
-Benchmark__100B	   10000	    523019 ns/op
-Benchmark___1KB	   10000	    605235 ns/op
-Benchmark__10KB	   10000	    957467 ns/op
-Benchmark_100KB	    5000	   4101891 ns/op
-Benchmark_N_RPCs____1_chunk_____1B	   10000	    554063 ns/op
-Benchmark_N_RPCs____1_chunk____10B	   10000	    551424 ns/op
-Benchmark_N_RPCs____1_chunk___100B	   10000	    538308 ns/op
-Benchmark_N_RPCs____1_chunk____1KB	   10000	    585579 ns/op
-Benchmark_N_RPCs____1_chunk___10KB	   10000	    904789 ns/op
-Benchmark_N_RPCs___10_chunks___1KB	   10000	   1460984 ns/op
-Benchmark_N_RPCs__100_chunks___1KB	    1000	   8491514 ns/op
-Benchmark_N_RPCs_1000_chunks___1KB	     100	 105269359 ns/op
-Benchmark_1_RPC_N_chunks_____1B	  200000	    763769 ns/op
-Benchmark_1_RPC_N_chunks____10B	  100000	    583134 ns/op
-Benchmark_1_RPC_N_chunks___100B	  100000	     80849 ns/op
-Benchmark_1_RPC_N_chunks____1KB	  100000	     88820 ns/op
-Benchmark_1_RPC_N_chunks___10KB	   50000	    361596 ns/op
-Benchmark_1_RPC_N_chunks__100KB	    5000	   3127193 ns/op
-ok  	veyron/runtimes/google/ipc/benchmarks	525.095s
+Benchmark____1B     2000     3895391 ns/op     0.00 MB/s
+Benchmark___10B     2000     3982372 ns/op     0.01 MB/s
+Benchmark___1KB     5000     3251297 ns/op     0.62 MB/s
+Benchmark_100KB     2000     6244664 ns/op    32.03 MB/s
+Benchmark____1_chunk_____1B     5000     4070866 ns/op     0.00 MB/s
+Benchmark____1_chunk____10B     2000     4242328 ns/op     0.00 MB/s
+Benchmark____1_chunk____1KB     2000     3679679 ns/op     0.54 MB/s
+Benchmark____1_chunk___10KB     2000     4070936 ns/op     4.91 MB/s
+Benchmark___10_chunks____1B     2000     3828552 ns/op     0.01 MB/s
+Benchmark___10_chunks___10B     5000     3685269 ns/op     0.05 MB/s
+Benchmark___10_chunks___1KB     2000     6831116 ns/op     2.93 MB/s
+Benchmark___10_chunks__10KB     1000     9662880 ns/op    20.70 MB/s
+Benchmark__100_chunks____1B     2000     8938980 ns/op     0.02 MB/s
+Benchmark__100_chunks___10B     2000     5924969 ns/op     0.34 MB/s
+Benchmark__100_chunks___1KB      500    37264103 ns/op     5.37 MB/s
+Benchmark__100_chunks__10KB      100    64999728 ns/op    30.77 MB/s
+Benchmark__per_chunk____1B    500000     1535312 ns/op     0.00 MB/s
+Benchmark__per_chunk___10B      2000     9416017 ns/op     0.00 MB/s
+Benchmark__per_chunk___1KB      1000     7803789 ns/op     0.26 MB/s
+Benchmark__per_chunk__10KB      1000     7828585 ns/op     2.55 MB/s
+Benchmark____1B_mux___10_chunks___10B     1000     9233379 ns/op     0.00 MB/s
+Benchmark____1B_mux___10_chunks___1KB     1000     8639613 ns/op     0.00 MB/s
+Benchmark____1B_mux__100_chunks___10B      500    30530925 ns/op     0.00 MB/s
+Benchmark____1B_mux__100_chunks___1KB      200    40886630 ns/op     0.00 MB/s
 
-
-The Benchmark_Simple_____1KB line shows that it takes an average of 0.605 ms to
+'Benchmark___1KB' shows that it takes an average of 3.251 ms to
 execute a simple Echo RPC with a 1 KB payload.
 
-The Benchmark_N_RPCs____1_chunk____1KB line shows that a streaming RPC with the
-same payload (i.e. 1 chunk of 1 KB) takes about 0.586 ms on average.
+'Benchmark___10_chunks___1KB' shows that a streaming RPC with the
+same payload (i.e. 10 chunks of 1 KB) takes about 6.831 ms on average.
 
-And Benchmark_1_RPC_N_chunks____1KB shows that sending a stream of 1 KB chunks
-takes an average of 0.088 ms per chunk.
+'Benchmark__per_chunk___1KB' shows that sending a stream of 1 KB chunks
+takes an average of 7.804 ms per chunk.
 
+'Benchmark____1B_mux___10_chunks___1KB' shows that it takes an average
+of 9.233 ms to execute a simple Echo RPC with a 1 B payload while streaming
+10 chunks of 1 KB payloads continuously in the same process.
+
+bm/main.go does the same benchmarks as ipc_test.go but with more varying
+configurations and optional histogram outputs.
+
+$ veyron go run veyron/runtimes/google/ipc/benchmarks/bm/main.go \
+  -test.cpu=1,2 -test.benchtime=5s -histogram
+
+RESULTS.txt has the latest benchmark results with main.go
+
+================================================================================
 
 Running the client and server as separate processes.
 
@@ -127,4 +143,3 @@ CallEcho 8 44995000
 CallEcho 9 53166000
 
 On the pi, the first RPC takes ~2.5 sec to execute.
-
--- runtimes/google/ipc/benchmarks/client.go
+++ runtimes/google/ipc/benchmarks/client.go
@@ -3,96 +3,168 @@ package benchmarks
 import (
 	"bytes"
 	"fmt"
-	"io"
+	"testing"
 	"time"
 
-	"veyron.io/veyron/veyron2"
+	"veyron.io/veyron/veyron/lib/testutil"
+
 	"veyron.io/veyron/veyron2/context"
 	"veyron.io/veyron/veyron2/vlog"
 )
 
-// CallEcho calls the Echo method 'iterations' times with the given payload
-// size, and optionally logs the result.
-func CallEcho(ctx context.T, address string, iterations, payloadSize int, log io.Writer) {
+// CallEcho calls 'Echo' method 'iterations' times with the given payload
+// size, and optionally updates the stats.
+func CallEcho(b *testing.B, ctx context.T, address string, iterations, payloadSize int, stats *testutil.BenchStats) {
+	stub := BenchmarkClient(address)
+	_, err := stub.Echo(ctx, make([]byte, 0)) // Create VC.
+	if err != nil {
+		vlog.Fatalf("Echo failed: %v", err)
+	}
+
 	payload := make([]byte, payloadSize)
-	for _, i := range payload {
+	for i := range payload {
 		payload[i] = byte(i & 0xff)
 	}
 
-	stub := BenchmarkClient(address)
+	if stats != nil {
+		stats.Clear()
+	}
+
+	b.SetBytes(int64(payloadSize) * 2) // 2 for round trip of each payload.
+	b.ResetTimer()                     // Exclude setup time from measurement.
+
 	for i := 0; i < iterations; i++ {
+		b.StartTimer()
 		start := time.Now()
-		result, err := stub.Echo(ctx, payload)
+
+		r, err := stub.Echo(ctx, payload)
+
 		elapsed := time.Since(start)
+		b.StopTimer()
+
 		if err != nil {
 			vlog.Fatalf("Echo failed: %v", err)
 		}
-		if !bytes.Equal(payload, result) {
-			vlog.Fatalf("Echo return different payload: got %v, expected %v", result, payload)
+		if !bytes.Equal(r, payload) {
+			vlog.Fatalf("Echo returned %v, but expected %v", r, payload)
 		}
-		if log != nil {
-			log.Write([]byte(fmt.Sprintf("CallEcho %d %d\n", i, elapsed)))
+
+		if stats != nil {
+			stats.Add(elapsed)
 		}
 	}
 }
 
-// CallEchoStream calls the EchoStream method 'rpcCount' times. Each iteration
-// sends 'messageCount' messages on the stream and receives the same number
-// back. Each message has the given payload size. Optionally logs the result.
-func CallEchoStream(runtime veyron2.Runtime, address string, rpcCount, messageCount, payloadSize int, log io.Writer) {
+// CallEchoStream calls 'EchoStream' method 'iterations' times. Each iteration
+// sends 'chunkCnt' chunks on the stream and receives the same number of chunks
+// back. Each chunk has the given payload size. Optionally updates the stats.
+func CallEchoStream(b *testing.B, ctx context.T, address string, iterations, chunkCnt, payloadSize int, stats *testutil.BenchStats) {
+	done, _ := StartEchoStream(b, ctx, address, iterations, chunkCnt, payloadSize, stats)
+	<-done
+}
+
+// StartEchoStream starts to call 'EchoStream' method 'iterations' times.
+// This does not block, and returns a channel that will receive the number
+// of iterations when it's done. It also returns a callback function to stop
+// the streaming. Each iteration requests 'chunkCnt' chunks on the stream and
+// receives that number of chunks back. Each chunk has the given payload size.
+// Optionally updates the stats. Zero 'iterations' means unlimited.
+func StartEchoStream(b *testing.B, ctx context.T, address string, iterations, chunkCnt, payloadSize int, stats *testutil.BenchStats) (<-chan int, func()) {
+	stub := BenchmarkClient(address)
+	_, err := stub.Echo(ctx, make([]byte, 0)) // Create VC.
+	if err != nil {
+		vlog.Fatalf("Echo failed: %v", err)
+	}
+
 	payload := make([]byte, payloadSize)
-	for _, i := range payload {
+	for i := range payload {
 		payload[i] = byte(i & 0xff)
 	}
 
-	stub := BenchmarkClient(address)
-	for i := 0; i < rpcCount; i++ {
-		start := time.Now()
-		ctx, _ := runtime.NewContext().WithTimeout(time.Hour)
-		stream, err := stub.EchoStream(ctx)
-		if err != nil {
-			vlog.Fatalf("EchoStream failed: %v", err)
+	if stats != nil {
+		stats.Clear()
+	}
+
+	done, stop := make(chan int, 1), make(chan struct{})
+	stopped := func() bool {
+		select {
+		case <-stop:
+			return true
+		default:
+			return false
 		}
-		done := make(chan error, 1)
-		go func() {
-			rStream := stream.RecvStream()
-			for rStream.Advance() {
-				chunk := rStream.Value()
-				if err == io.EOF {
-					done <- nil
-					return
-				}
-				if err != nil {
-					done <- err
-					return
-				}
-				if !bytes.Equal(payload, chunk) {
-					done <- fmt.Errorf("Recv got different payload: got %v, expected %v", chunk, payload)
-					return
-				}
+	}
+
+	if b.N > 0 {
+		// 2 for round trip of each payload.
+		b.SetBytes(int64((iterations*chunkCnt/b.N)*payloadSize) * 2)
+	}
+	b.ResetTimer() // Exclude setup time from measurement.
+
+	go func() {
+		defer close(done)
+
+		n := 0
+		for ; !stopped() && (iterations == 0 || n < iterations); n++ {
+			b.StartTimer()
+			start := time.Now()
+
+			stream, err := stub.EchoStream(ctx)
+			if err != nil {
+				vlog.Fatalf("EchoStream failed: %v", err)
 			}
 
-			done <- rStream.Err()
-		}()
-		sender := stream.SendStream()
-		for j := 0; j < messageCount; j++ {
-			if err = sender.Send(payload); err != nil {
-				vlog.Fatalf("Send failed: %v", err)
+			rDone := make(chan error, 1)
+			go func() {
+				defer close(rDone)
+
+				rStream := stream.RecvStream()
+				i := 0
+				for ; rStream.Advance(); i++ {
+					r := rStream.Value()
+					if !bytes.Equal(r, payload) {
+						rDone <- fmt.Errorf("EchoStream returned %v, but expected %v", r, payload)
+						return
+					}
+				}
+				if i != chunkCnt {
+					rDone <- fmt.Errorf("EchoStream returned %d chunks, but expected %d", n, chunkCnt)
+					return
+				}
+				rDone <- rStream.Err()
+			}()
+
+			sStream := stream.SendStream()
+			for i := 0; i < chunkCnt; i++ {
+				if err = sStream.Send(payload); err != nil {
+					vlog.Fatalf("EchoStream Send failed: %v", err)
+				}
+			}
+			if err = sStream.Close(); err != nil {
+				vlog.Fatalf("EchoStream Send failed: %v", err)
+			}
+
+			if err = <-rDone; err != nil {
+				vlog.Fatalf("%v", err)
+			}
+
+			if err = stream.Finish(); err != nil {
+				vlog.Fatalf("Finish failed: %v", err)
+			}
+
+			elapsed := time.Since(start)
+			b.StopTimer()
+
+			if stats != nil {
+				stats.Add(elapsed)
 			}
 		}
-		if err = sender.Close(); err != nil {
-			vlog.Fatalf("Close() failed: %v", err)
-		}
-		if err = <-done; err != nil {
-			vlog.Fatalf("%v", err)
-		}
 
-		if err = stream.Finish(); err != nil {
-			vlog.Fatalf("Finish failed: %v", err)
-		}
-		elapsed := time.Since(start)
-		if log != nil {
-			log.Write([]byte(fmt.Sprintf("CallEchoStream %d %d\n", i, elapsed)))
-		}
+		done <- n
+	}()
+
+	return done, func() {
+		close(stop)
+		<-done
 	}
 }
