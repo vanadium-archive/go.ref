ba35a6bf097989de96d441a6dbef55d0078d1fb4
Cosmos Nicolaou <cnicolaou@google.com>
2014-12-24 07:48:39 -0800

    veyron/lib/websocket: fix build CL 8510.
    Change-Id: I5258b6c92682edfd71d1b34b22d6ec076e790df5

--- lib/websocket/listener_nacl.go
+++ lib/websocket/listener_nacl.go
@@ -9,6 +9,9 @@ import (
 
 // Websocket listeners are not supported in NaCl.
 // This file is needed for compilation only.
+func listener(protocol, address string, hybrid bool) (net.Listener, error) {
+	return nil, fmt.Errorf("Websocket Listener called in nacl code!")
+}
 
 func Listener(protocol, address string) (net.Listener, error) {
 	return nil, fmt.Errorf("Websocket Listener called in nacl code!")
