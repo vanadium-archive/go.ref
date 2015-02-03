90d21dc20f0098529c5dd1f0806861700c3ed945
James Ring <sjr@jdns.org>
2015-02-02 18:10:38 -0800

    veyron/lib/testutil/integration: add BinaryFromPath function to TestEnvironment
    Change-Id: I0c95dfa40dc314ac2fc5a795e1c11862f01c1f29

---
+++ lib/testutil/integration/testdata/integration_test.go
@@ -0,0 +1,28 @@
+package testdata
+
+import (
+	"testing"
+
+	"v.io/core/veyron/lib/modules"
+	"v.io/core/veyron/lib/testutil/integration"
+	_ "v.io/core/veyron/profiles"
+)
+
+func TestHelperProcess(t *testing.T) {
+	modules.DispatchInTest()
+}
+
+func TestBinaryFromPath(t *testing.T) {
+	env := integration.NewTestEnvironment(t)
+	defer env.Cleanup()
+
+	bash := env.BinaryFromPath("/bin/bash")
+	if want, got := "hello world\n", bash.Start("-c", "echo hello world").Output(); want != got {
+		t.Fatalf("unexpected output, want %s, got %s", want, got)
+	}
+
+	// TODO(sjr): revive this once stderr handling is fixed.
+	// if want, got := "hello world\n", bash.Start("-c", "echo hello world 1>&2").ErrorOutput(); want != got {
+	//	t.Fatalf("unexpected output, want %s, got %s", want, got)
+	// }
+}
