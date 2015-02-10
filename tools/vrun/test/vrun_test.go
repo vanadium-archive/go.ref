8c3f1f983e79d914292297d960ab57a9b0e68a56
James Ring <sjr@google.com>
2015-02-10 15:46:32 -0800

    veyron/tools/vrun: port vrun test.sh to go
    Change-Id: I618555381c7eb0ca96aec68d3a27f5f0e41b4ce4

---
+++ tools/vrun/test/vrun_test.go
@@ -0,0 +1,26 @@
+package main_test
+
+//go:generate v23 test generate .
+
+import (
+	"os"
+
+	"v.io/core/veyron/lib/testutil/v23tests"
+	_ "v.io/core/veyron/profiles/static"
+)
+
+func V23TestAgentd(t v23tests.T) {
+	vrunBin := t.BuildGoPkg("v.io/core/veyron/tools/vrun")
+	pingpongBin := t.BuildGoPkg("v.io/core/veyron/security/agent/pingpong")
+	agentdBin := t.BuildGoPkg("v.io/core/veyron/security/agent/agentd")
+	helperBin := t.BuildGoPkg("v.io/core/veyron/tools/vrun/test")
+	principalBin := t.BuildGoPkg("v.io/core/veyron/tools/principal")
+
+	creds := t.TempDir()
+	agentdBin.WithEnv("VEYRON_CREDENTIALS="+creds).Start("--no_passphrase",
+		"--additional_principals="+creds,
+		helperBin.Path(),
+		vrunBin.Path(),
+		pingpongBin.Path(),
+		principalBin.Path()).WaitOrDie(os.Stdout, os.Stderr)
+}
