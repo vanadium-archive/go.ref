273494696341d1487df99d7d081dcb68f50ebf24
Suharsh Sivakumar <suharshs@google.com>
2015-02-27 15:01:54 -0800

    veyron/profiles/internal/platform: Move v23.Platform to to be only internal.
    Since there are currently no users of this.
    
    MultiPart: 2/2
    Change-Id: Ia00cb0ac04697cad4dbe71d14e0561bf21346bf9

--- profiles/internal/platform/platform_darwin.go
+++ profiles/internal/platform/platform_darwin.go
@@ -4,21 +4,17 @@ package platform
 // #include <errno.h>
 import "C"
 
-import (
-	"fmt"
+import "fmt"
 
-	"v.io/v23"
-)
-
-// Platform returns the description of the Platform this process is running on.
-// A default value for v23.Platform is provided even if an error is
+// GetPlatform returns the description of the Platform this process is running on.
+// A default value for Platform is provided even if an error is
 // returned; nil is never returned for the first return result.
-func Platform() (*v23.Platform, error) {
+func GetPlatform() (*Platform, error) {
 	var t C.struct_utsname
 	if r, err := C.uname(&t); r != 0 {
-		return &v23.Platform{}, fmt.Errorf("uname failed: errno %d", err)
+		return &Platform{}, fmt.Errorf("uname failed: errno %d", err)
 	}
-	d := &v23.Platform{
+	d := &Platform{
 		Vendor:  "google",
 		Model:   "generic",
 		System:  C.GoString(&t.sysname[0]),
