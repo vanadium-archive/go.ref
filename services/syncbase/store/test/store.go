96f9eb74711edcc6ca5d50af9996302e893a4db8
Sergey Rogulenko <rogulenko@google.com>
2015-05-19 20:12:07 -0700

    TBR: rename file test.go -> store.go
    Change-Id: I39d033c7c54bca51221e63bc507ca67659b42930

--- services/syncbase/store/test/store.go
+++ services/syncbase/store/test/store.go
@@ -2,7 +2,6 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// TODO(rogulenko): rename this file test.go -> store.go.
 package test
 
 import (
