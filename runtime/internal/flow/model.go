67506bcd85e4efaa53134a095ee0c7ef5011def7
Suharsh Sivakumar <suharshs@google.com>
2015-06-26 15:29:06 -0700

    ref/runtime/internal/flow: Connection sharing flow.Manager API.
    This change introduces modifications to the flow.Manager API to allow
    connections to be shared between servers and clients on the same Manager.
    
    In particular, Manager.Listen listens for all Acceptors of the manager.
    Each Server should call Manager.Acceptor to receive an Acceptor unique
    to their server (i.e. the RoutingID of the Acceptor will be unique).
    Additional calls to Manager.Dial also automatically Listen on outgoing
    connections.
    
    Change-Id: I39b09710c6ca3dedd4a9361a11ff82d066d93f2e

--- runtime/internal/flow/model.go
+++ runtime/internal/flow/model.go
@@ -13,24 +13,39 @@ import (
 
 // Manager is the interface for managing the creation of Flows.
 type Manager interface {
-	// Listen creates a Listener that can be used to listen on addresses
-	// and Accept flows initiates to those addresses.
+	// Acceptor creates a Acceptor that can be used to accept new flows from addresses
+	// that the Manager is listening on.
 	//
 	// For example:
-	//   ln, err := sm.NewListener(ctx, blessings)
-	//   ep, err := ln.Listen("tcp", ":0")
+	//   err := m.Listen(ctx, "tcp", ":0")
+	//   a, err := m.Acceptor(ctx, blessings)
 	//   for {
-	//     flow, err := ln.Accept(ctx)
+	//     flow, err := a.Accept(ctx)
 	//     // process flow
 	//   }
-	// can be used to accept Flows initiated by remote processes to the endpoint
-	// returned by ln.Listen.
 	//
-	// blessings are the Blessings presented to the Client during authentication.
-	NewListener(ctx *context.T, blessings security.Blessings) (Listener, error)
+	// can be used to accept Flows initiated by remote processes to the endpoints returned
+	// by Acceptor.Endpoints. All created Acceptors accept Flows from all addresses the
+	// Manager is listening on, but are differentiated by a RoutingID unique to each
+	// Acceptor.
+	//
+	// 'blessings' are the Blessings presented to the Client during authentication.
+	Acceptor(ctx *context.T, blessings security.Blessings) (Acceptor, error)
 
-	// Dial creates a Flow to the provided remote endpoint.
-	Dial(ctx *context.T, remote naming.Endpoint) (Flow, error)
+	// Listen causes the Manager to accept flows from the provided protocol and address.
+	// Listen may be called muliple times.
+	Listen(ctx *context.T, protocol, address string) error
+
+	// Dial creates a Flow to the provided remote endpoint. 'auth' is used to authorize
+	// the remote end.
+	//
+	// To maximize re-use of connections, the Manager will also Listen on Dialed
+	// connections for the lifetime of the connection.
+	//
+	// TODO(suharshs): Revisit passing in an authorizer here. Perhaps restrict server
+	// authorization to a smaller set of policies, or use a different mechanism to
+	// allow the user to specify a policy.
+	Dial(ctx *context.T, remote naming.Endpoint, auth security.Authorizer) (Flow, error)
 
 	// Closed returns a channel that remains open for the lifetime of the Manager
 	// object. Once the channel is closed any operations on the Manager will
@@ -52,26 +67,28 @@ type Flow interface {
 	RemoteBlessings() security.Blessings
 	// LocalDischarges returns the discharges presented by the local end of the flow during authentication.
 	//
-	// The discharges are organized in a map keyed by the discharge-identifier.
+	// Discharges are organized in a map keyed by the discharge-identifier.
 	LocalDischarges() map[string]security.Discharge
 	// RemoteDischarges returns the discharges presented by the remote end of the flow during authentication.
 	//
-	// The discharges are organized in a map keyed by the discharge-identifier.
+	// Discharges are organized in a map keyed by the discharge-identifier.
 	RemoteDischarges() map[string]security.Discharge
 
 	// Closed returns a channel that remains open until the flow has been closed or
-	// the ctx to the Dail or Accept call used to create the flow has been cancelled.
+	// the ctx to the Dial or Accept call used to create the flow has been cancelled.
 	Closed() <-chan struct{}
 }
 
-// Listener is the interface for accepting Flows created by a remote process.
-type Listener interface {
-	// Listen listens on the protocol and address specified. This may be called
-	// multiple times.
-	Listen(protocol, address string) (naming.Endpoint, error)
+// Acceptor is the interface for accepting Flows created by a remote process.
+type Acceptor interface {
+	// ListeningEndpoints returns the endpoints that the Manager has explicitly
+	// listened on. The Acceptor will accept new flows on these endpoints.
+	// Returned endpoints all have a RoutingID unique to the Acceptor.
+	ListeningEndpoints() []naming.Endpoint
 	// Accept blocks until a new Flow has been initiated by a remote process.
 	Accept(ctx *context.T) (Flow, error)
-	// Closed returns a channel that remains open until the Listener has been closed.
-	// i.e. the context provided to Manager.NewListener() has been cancelled.
+	// Closed returns a channel that remains open until the Acceptor has been closed.
+	// i.e. the context provided to Manager.Acceptor() has been cancelled. Once the
+	// channel is closed, all calls to Accept will result in an error.
 	Closed() <-chan struct{}
 }
