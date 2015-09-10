0639741a4a7a0dbd8769dc915ed71133986872ee
Todd Wang <toddw@google.com>
2015-09-10 14:51:34 -0700

    TBR services/device/dmrun: Fix copyright
    Change-Id: I8cffbdacbeb3a8d7d07be888a1cdbc2e0fa8e6d5

--- services/device/dmrun/dmrun.go
+++ services/device/dmrun/dmrun.go
@@ -1,3 +1,7 @@
+// Copyright 2015 The Vanadium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 // Command dmrun runs a binary on a remote GCE instance using device manager.
 //
 // dmrun creates the GCE instance, installs and starts device manager on it, and
