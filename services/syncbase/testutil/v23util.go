464445b6fb6a47e3a30550cb4c877a4ee4a30ba3
Ken Ashcraft <kash@google.com>
2015-11-17 13:35:25 -0800

    syncbase: StartSyncbased() improvements.
    - Add a version of StartSyncbased() that returns a cleanup method
      that takes a signal.  This allows tests to SIGKILL syncbased instead
      of the default SIGINT.
    - Block until the syncbase service is running so that client tests
      don't need to sleep.
    
    MultiPart: 2/2
    
    Change-Id: Iedd8512415cb1f05e6a486004579367055e26eb1

--- services/syncbase/testutil/v23util.go
+++ services/syncbase/testutil/v23util.go
@@ -12,14 +12,30 @@ import (
 	"runtime/debug"
 	"syscall"
 
+	"v.io/v23"
+	"v.io/v23/syncbase"
+
 	"v.io/x/ref/test/modules"
 	"v.io/x/ref/test/v23tests"
 )
 
 // StartSyncbased starts a syncbased process, intended to be accessed from an
 // integration test (run using --v23.tests). The returned cleanup function
-// should be called once the syncbased process is no longer needed.
+// should be called once the syncbased process is no longer needed.  See
+// StartKillableSyncbased for killing the syncbase with an arbitrary signal.
 func StartSyncbased(t *v23tests.T, creds *modules.CustomCredentials, name, rootDir, permsLiteral string) (cleanup func()) {
+	f := StartKillableSyncbased(t, creds, name, rootDir, permsLiteral)
+	return func() {
+		f(syscall.SIGINT)
+	}
+}
+
+// StartKillableSyncbased starts a syncbased process, intended to be accessed from an
+// integration test (run using --v23.tests). The returned cleanup function
+// should be called once the syncbased process is no longer needed.
+func StartKillableSyncbased(t *v23tests.T, creds *modules.CustomCredentials,
+	name, rootDir, permsLiteral string) (cleanup func(signal syscall.Signal)) {
+
 	syncbased := t.BuildV23Pkg("v.io/x/ref/services/syncbase/syncbased")
 	// Create root dir for the store.
 	rmRootDir := false
@@ -40,12 +56,13 @@ func StartSyncbased(t *v23tests.T, creds *modules.CustomCredentials, name, rootD
 		"--v23.permissions.literal", permsLiteral,
 		"--name="+name,
 		"--root-dir="+rootDir)
-	return func() {
+	RunClient(t, creds, runWaitForService, name)
+	return func(signal syscall.Signal) {
 		// TODO(sadovsky): Something's broken here. If the syncbased invocation
 		// fails (e.g. if NewService returns an error), currently it's possible for
 		// the test to fail without the crash error getting logged. This makes
 		// debugging a challenge.
-		go invocation.Kill(syscall.SIGINT)
+		go invocation.Kill(signal)
 		stdout, stderr := bytes.NewBuffer(nil), bytes.NewBuffer(nil)
 		if err := invocation.Shutdown(stdout, stderr); err != nil {
 			log.Printf("syncbased terminated with an error: %v\nstdout: %v\nstderr: %v\n", err, stdout, stderr)
@@ -61,6 +78,25 @@ func StartSyncbased(t *v23tests.T, creds *modules.CustomCredentials, name, rootD
 		}
 	}
 }
+
+// runWaitForService issues a noop rpc to force this process to wait until the
+// server is ready to accept requests.  Without this, calls to glob will
+// silently return no results if the service is not responding (i.e. ListApps,
+// ListDatabases return the empty set).
+// TODO(kash): sadovsky says that there is some other mechanism in the modules
+// framework that doesn't involve RPCs, involving instrumenting the server to
+// print some indication that it's ready (and detecting that from the parent
+// process).
+var runWaitForService = modules.Register(func(env *modules.Env, args ...string) error {
+	ctx, shutdown := v23.Init()
+	defer shutdown()
+	s := syncbase.NewService(args[0])
+	a := s.App("dummyApp")
+	if _, err := a.Exists(ctx); err != nil {
+		return err
+	}
+	return nil
+}, "runWaitForService")
 
 // RunClient runs the given program and waits until it terminates.
 func RunClient(t *v23tests.T, creds *modules.CustomCredentials, program modules.Program, args ...string) {
