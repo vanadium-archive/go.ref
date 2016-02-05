bbf881f938c04d103ce8d6a9fbca3531e6fc54bc
Suharsh Sivakumar <suharshs@google.com>
2016-02-05 11:48:01 -0800

    benchmark: Add ProxiedServer benchmark.
    Here are the results of all single byte rpcs for comparison.
    Benchmark____1B-12              	   50000	    377124 ns/op	   0.01 MB/s
    Benchmark____1B_ClientTPCav-12  	   20000	    686344 ns/op	   0.00 MB/s
    Benchmark____1B_ServerTPCav-12  	   20000	    726849 ns/op	   0.00 MB/s
    Benchmark____1B_ProxiedServer-12	   30000	    492016 ns/op	   0.00 MB/s
    
    Change-Id: I813eb6ee9a5ccb001767724ff89541332c307d33

--- runtime/internal/rpc/benchmark/benchmark_test.go
+++ runtime/internal/rpc/benchmark/benchmark_test.go
@@ -10,17 +10,19 @@ import (
 
 	"v.io/v23"
 	"v.io/v23/context"
+	"v.io/v23/rpc"
 	lsecurity "v.io/x/ref/lib/security"
 	"v.io/x/ref/lib/security/securityflag"
 	_ "v.io/x/ref/runtime/factories/roaming"
 	"v.io/x/ref/runtime/internal/rpc/benchmark/internal"
+	"v.io/x/ref/services/xproxy/xproxy"
 	"v.io/x/ref/test"
 	"v.io/x/ref/test/benchmark"
 )
 
 var (
-	serverAddr, tpcServerAddr string
-	ctx, tpcCtx               *context.T
+	serverAddr, tpcServerAddr, proxiedServerAddr string
+	ctx, tpcCtx                                  *context.T
 )
 
 // Benchmarks for non-streaming RPC.
@@ -58,6 +60,18 @@ func Benchmark__100B_ServerTPCav(b *testing.B) { runEchoServerTPC(b, 100) }
 func Benchmark___1KB_ServerTPCav(b *testing.B) { runEchoServerTPC(b, 1000) }
 func Benchmark__10KB_ServerTPCav(b *testing.B) { runEchoServerTPC(b, 10000) }
 func Benchmark_100KB_ServerTPCav(b *testing.B) { runEchoServerTPC(b, 100000) }
+
+// Benchmark for non-streaming RPC with a proxied Server.
+func runEchoProxiedServer(b *testing.B, payloadSize int) {
+	internal.CallEcho(b, ctx, proxiedServerAddr, b.N, payloadSize, benchmark.AddStats(b, 16))
+}
+
+func Benchmark____1B_ProxiedServer(b *testing.B) { runEchoProxiedServer(b, 1) }
+func Benchmark___10B_ProxiedServer(b *testing.B) { runEchoProxiedServer(b, 10) }
+func Benchmark__100B_ProxiedServer(b *testing.B) { runEchoProxiedServer(b, 100) }
+func Benchmark___1KB_ProxiedServer(b *testing.B) { runEchoProxiedServer(b, 1000) }
+func Benchmark__10KB_ProxiedServer(b *testing.B) { runEchoProxiedServer(b, 10000) }
+func Benchmark_100KB_ProxiedServer(b *testing.B) { runEchoProxiedServer(b, 100000) }
 
 // Benchmarks for streaming RPC.
 func runEchoStream(b *testing.B, chunkCnt, payloadSize int) {
@@ -136,13 +150,27 @@ func TestMain(m *testing.M) {
 	var shutdown v23.Shutdown
 	ctx, shutdown = test.V23Init()
 
+	setupServerClient(ctx)
+	setupThirdPartyCaveatServerClient(ctx)
+	setupProxiedServerClient(ctx)
+
+	r := benchmark.RunTestMain(m)
+	shutdown()
+	os.Exit(r)
+}
+
+func setupServerClient(ctx *context.T) {
 	// Server with no ThirdPartyCaveat.
 	_, server, err := v23.WithNewServer(ctx, "", internal.NewService(), securityflag.NewAuthorizerOrDie())
 	if err != nil {
 		ctx.Fatalf("NewServer failed: %v", err)
 	}
 	serverAddr = server.Status().Endpoints[0].Name()
+	// Create a Conn to exclude the Conn setup time from the benchmark.
+	internal.CallEcho(&testing.B{}, ctx, serverAddr, 1, 0, benchmark.NewStats(1))
+}
 
+func setupThirdPartyCaveatServerClient(ctx *context.T) {
 	// Create a DischargeServer and a principal with a corresponding ThirdPartyCaveat.
 	tpcav := internal.NewDischargeServer(ctx)
 	p := v23.GetPrincipal(ctx)
@@ -167,12 +195,32 @@ func TestMain(m *testing.M) {
 	}
 	tpcServerAddr = tpcServer.Status().Endpoints[0].Name()
 
-	// Create a VC to exclude the VC setup time from the benchmark.
-	internal.CallEcho(&testing.B{}, ctx, serverAddr, 1, 0, benchmark.NewStats(1))
+	// Create Conns to exclude the Conn setup time from the benchmark.
 	internal.CallEcho(&testing.B{}, tpcCtx, serverAddr, 1, 0, benchmark.NewStats(1))
 	internal.CallEcho(&testing.B{}, ctx, tpcServerAddr, 1, 0, benchmark.NewStats(1))
+}
 
-	r := benchmark.RunTestMain(m)
-	shutdown()
-	os.Exit(r)
+func setupProxiedServerClient(ctx *context.T) {
+	// Create a proxy.
+	pxy, err := xproxy.New(ctx, "", securityflag.NewAuthorizerOrDie())
+	if err != nil {
+		ctx.Fatal(err)
+	}
+	pname := pxy.ListeningEndpoints()[0].Name()
+	// Create a server that listens on the proxy.
+	pCtx := v23.WithListenSpec(ctx, rpc.ListenSpec{Proxy: pname})
+	_, proxiedServer, err := v23.WithNewServer(pCtx, "", internal.NewService(), securityflag.NewAuthorizerOrDie())
+	if err != nil {
+		ctx.Fatal(err)
+	}
+	for {
+		status := proxiedServer.Status()
+		if status.Endpoints[0].Addr().Network() != "bidi" {
+			proxiedServerAddr = status.Endpoints[0].Name()
+			break
+		}
+		<-status.Valid
+	}
+	// Create Conns to exclude the Conn setup time from the benchmark.
+	internal.CallEcho(&testing.B{}, ctx, proxiedServerAddr, 1, 0, benchmark.NewStats(1))
 }
