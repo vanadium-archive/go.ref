5280e8e7ccfc21d4cdd62b8957fd634eb8b9f5a0
Matt Rosencrantz <mattr@google.com>
2016-03-07 10:22:13 -0800

    testing: Add a flag "--v23.debug-address".  When specified it starts a debug http server when the test is finished and causes the test to hang until Ctrl-C so the user can interact with it.
    Change-Id: Ia0b03f56a216bb1b2cfe4b3b1136cb4e9309bac9

--- test/init.go
+++ test/init.go
@@ -7,9 +7,22 @@
 package test
 
 import (
+	"flag"
+
+	"os"
+	"os/signal"
+	"time"
 	"v.io/v23"
 	"v.io/v23/context"
+	"v.io/v23/rpc"
+	"v.io/v23/security"
+	"v.io/x/ref/services/debug/debug/browseserver"
 )
+
+var debugOnShutdown = flag.String("v23.debug-address", "",
+	"If this is set then when a test runs, we start a debug browser (serving at the given address)"+
+		" and hang so you can look at it. This only works when running a single test,"+
+		" because you have to ctrl-c to finish the test.")
 
 // V23Init initializes the runtime and sets up the principal with a self-signed
 // TestBlessing. The blessing setup step is skipped if this function is invoked
@@ -21,6 +34,12 @@ import (
 func V23Init() (*context.T, v23.Shutdown) {
 	ctx, shutdown := v23.Init()
 	ctx = internalInit(ctx, false)
+
+	if *debugOnShutdown != "" {
+		orig := shutdown
+		shutdown = func() { debug(ctx, orig) }
+	}
+
 	return ctx, shutdown
 }
 
@@ -32,5 +51,39 @@ func V23Init() (*context.T, v23.Shutdown) {
 func V23InitWithMounttable() (*context.T, v23.Shutdown) {
 	ctx, shutdown := v23.Init()
 	ctx = internalInit(ctx, true)
+
+	if *debugOnShutdown != "" {
+		orig := shutdown
+		shutdown = func() { debug(ctx, orig) }
+	}
+
 	return ctx, shutdown
+}
+
+func debug(ctx *context.T, shutdown func()) {
+	ctx, cancel := context.WithCancel(ctx)
+	ctx, s, err := v23.WithNewServer(ctx, "", &dummyService{}, security.AllowEveryone())
+	if err != nil {
+		panic(err)
+	}
+	eps := s.Status().Endpoints
+
+	go func() {
+		ch := make(chan os.Signal)
+		signal.Notify(ch, os.Interrupt)
+		<-ch
+		signal.Stop(ch)
+		cancel()
+	}()
+
+	if err := browseserver.Serve(ctx, *debugOnShutdown, eps[0].Name(), 10*time.Second, false); err != nil {
+		ctx.Infof("Stopped debug server: %v", err)
+	}
+	shutdown()
+}
+
+type dummyService struct{}
+
+func (*dummyService) Do(ctx *context.T, call rpc.ServerCall) error {
+	return nil
 }
