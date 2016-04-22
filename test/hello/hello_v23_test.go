32d8b2855e7adad987aa30e2be756736fb103459
Bogdan Caprita <caprita@google.com>
2016-04-21 17:12:47 -0700

    test: replace agentd by v23agentd
    Towards deprecating agentd.
    
    Change-Id: If72a615334f2460ae242381208816a6da1ff8605

--- test/hello/hello_v23_test.go
+++ test/hello/hello_v23_test.go
@@ -23,9 +23,14 @@ func withCreds(dir string, c *v23test.Cmd) *v23test.Cmd {
 	return c
 }
 
+func withCredsSock(sock string, c *v23test.Cmd) *v23test.Cmd {
+	c.Vars[ref.EnvAgentPath] = sock
+	return c
+}
+
 // setupCreds makes a bunch of credentials directories.
 // We do this ourselves instead of using v23test's credentials APIs because we
-// want to use the actual agentd binary, so that for regression tests we can
+// want to use the actual v23agentd binary, so that for regression tests we can
 // test against old agents.
 func setupCreds(sh *v23test.Shell, names ...string) (map[string]string, error) {
 	idp := testutil.NewIDProvider("root")
@@ -42,6 +47,20 @@ func setupCreds(sh *v23test.Shell, names ...string) (map[string]string, error) {
 		out[name] = dir
 	}
 	return out, nil
+}
+
+func startAgent(t *testing.T, sh *v23test.Shell, agentdbin, creds string) string {
+	agent := sh.Cmd(agentdbin, creds)
+	agent.Start()
+	agentSock := agent.S.ExpectVar(ref.EnvAgentPath)
+	if agent.S.Failed() {
+		t.Fatalf("Could not get %s: %v", ref.EnvAgentPath, agent.S.Error())
+	}
+	return agentSock
+}
+
+func withAgentCreds(t *testing.T, sh *v23test.Shell, agentdbin, creds string, c *v23test.Cmd) *v23test.Cmd {
+	return withCredsSock(startAgent(t, sh, agentdbin, creds), c)
 }
 
 func TestV23HelloDirect(t *testing.T) {
@@ -74,7 +93,7 @@ func TestV23HelloAgentd(t *testing.T) {
 	if err != nil {
 		t.Fatalf("Could not create credentials: %v", err)
 	}
-	agentdbin := v23test.BuildGoPkg(sh, "v.io/x/ref/services/agent/agentd")
+	agentdbin := v23test.BuildGoPkg(sh, "v.io/x/ref/services/agent/v23agentd")
 	serverbin := v23test.BuildGoPkg(sh, "v.io/x/ref/test/hello/helloserver")
 	clientbin := v23test.BuildGoPkg(sh, "v.io/x/ref/test/hello/helloclient")
 
@@ -84,7 +103,7 @@ func TestV23HelloAgentd(t *testing.T) {
 	if server.S.Failed() {
 		t.Fatalf("Could not get SERVER_NAME: %v", server.S.Error())
 	}
-	withCreds(creds["helloclient"], sh.Cmd(agentdbin, clientbin, "--name", name)).Run()
+	withAgentCreds(t, sh, agentdbin, creds["helloclient"], sh.Cmd(clientbin, "--name", name)).Run()
 }
 
 func TestV23HelloMounttabled(t *testing.T) {
@@ -96,20 +115,20 @@ func TestV23HelloMounttabled(t *testing.T) {
 	if err != nil {
 		t.Fatalf("Could not create credentials: %v", err)
 	}
-	agentdbin := v23test.BuildGoPkg(sh, "v.io/x/ref/services/agent/agentd")
+	agentdbin := v23test.BuildGoPkg(sh, "v.io/x/ref/services/agent/v23agentd")
 	mounttabledbin := v23test.BuildGoPkg(sh, "v.io/x/ref/services/mounttable/mounttabled")
 	serverbin := v23test.BuildGoPkg(sh, "v.io/x/ref/test/hello/helloserver")
 	clientbin := v23test.BuildGoPkg(sh, "v.io/x/ref/test/hello/helloclient")
 
 	name := "hello"
-	mounttabled := withCreds(creds["mounttabled"], sh.Cmd(agentdbin, mounttabledbin, "--v23.tcp.address", "127.0.0.1:0"))
+	mounttabled := withAgentCreds(t, sh, agentdbin, creds["mounttabled"], sh.Cmd(mounttabledbin, "--v23.tcp.address", "127.0.0.1:0"))
 	mounttabled.Start()
 	mtname := mounttabled.S.ExpectVar("NAME")
 	if mounttabled.S.Failed() {
 		t.Fatalf("Could not get NAME: %v", mounttabled.S.Error())
 	}
-	withCreds(creds["helloserver"], sh.Cmd(agentdbin, serverbin, "--name", name, "--v23.namespace.root", mtname)).Start()
-	withCreds(creds["helloclient"], sh.Cmd(agentdbin, clientbin, "--name", name, "--v23.namespace.root", mtname)).Run()
+	withAgentCreds(t, sh, agentdbin, creds["helloserver"], sh.Cmd(serverbin, "--name", name, "--v23.namespace.root", mtname)).Start()
+	withAgentCreds(t, sh, agentdbin, creds["helloclient"], sh.Cmd(clientbin, "--name", name, "--v23.namespace.root", mtname)).Run()
 }
 
 func TestV23HelloProxy(t *testing.T) {
@@ -121,23 +140,23 @@ func TestV23HelloProxy(t *testing.T) {
 	if err != nil {
 		t.Fatalf("Could not create credentials: %v", err)
 	}
-	agentdbin := v23test.BuildGoPkg(sh, "v.io/x/ref/services/agent/agentd")
+	agentdbin := v23test.BuildGoPkg(sh, "v.io/x/ref/services/agent/v23agentd")
 	mounttabledbin := v23test.BuildGoPkg(sh, "v.io/x/ref/services/mounttable/mounttabled")
 	xproxydbin := v23test.BuildGoPkg(sh, "v.io/x/ref/services/xproxy/xproxyd")
 	serverbin := v23test.BuildGoPkg(sh, "v.io/x/ref/test/hello/helloserver")
 	clientbin := v23test.BuildGoPkg(sh, "v.io/x/ref/test/hello/helloclient")
 
 	name := "hello"
-	mounttabled := withCreds(creds["mounttabled"], sh.Cmd(agentdbin, mounttabledbin, "--v23.tcp.address", "127.0.0.1:0"))
+	mounttabled := withAgentCreds(t, sh, agentdbin, creds["mounttabled"], sh.Cmd(mounttabledbin, "--v23.tcp.address", "127.0.0.1:0"))
 	mounttabled.Start()
 	mtname := mounttabled.S.ExpectVar("NAME")
 	if mounttabled.S.Failed() {
 		t.Fatalf("Could not get NAME: %v", mounttabled.S.Error())
 	}
 	proxyname := "proxy"
-	withCreds(creds["xproxyd"], sh.Cmd(agentdbin, xproxydbin, "--name", proxyname, "--v23.tcp.address", "127.0.0.1:0", "--v23.namespace.root", mtname, "--access-list", "{\"In\":[\"root\"]}")).Start()
-	withCreds(creds["helloserver"], sh.Cmd(agentdbin, serverbin, "--name", name, "--v23.proxy", proxyname, "--v23.tcp.address", "", "--v23.namespace.root", mtname)).Start()
-	withCreds(creds["helloclient"], sh.Cmd(agentdbin, clientbin, "--name", name, "--v23.proxy", proxyname, "--v23.tcp.address", "", "--v23.namespace.root", mtname)).Run()
+	withAgentCreds(t, sh, agentdbin, creds["xproxyd"], sh.Cmd(xproxydbin, "--name", proxyname, "--v23.tcp.address", "127.0.0.1:0", "--v23.namespace.root", mtname, "--access-list", "{\"In\":[\"root\"]}")).Start()
+	withAgentCreds(t, sh, agentdbin, creds["helloserver"], sh.Cmd(serverbin, "--name", name, "--v23.proxy", proxyname, "--v23.tcp.address", "", "--v23.namespace.root", mtname)).Start()
+	withAgentCreds(t, sh, agentdbin, creds["helloclient"], sh.Cmd(clientbin, "--name", name, "--v23.proxy", proxyname, "--v23.tcp.address", "", "--v23.namespace.root", mtname)).Run()
 }
 
 func TestMain(m *testing.M) {
