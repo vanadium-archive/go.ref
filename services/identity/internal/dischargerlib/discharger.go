16d23accdf99f3426849a36db9381c4402c9c10d
Matt Rosencrantz <mattr@google.com>
2016-04-29 11:33:53 -0700

    Discharger: Switch the default discharge expiry to 24 hours.
    Change-Id: I04f0b2de86847faf9247dfda3cea4e35dad57b88

--- services/identity/internal/dischargerlib/discharger.go
+++ services/identity/internal/dischargerlib/discharger.go
@@ -14,6 +14,8 @@ import (
 	"v.io/x/ref/services/discharger"
 )
 
+const dischargeExpiryTime = 24 * time.Hour
+
 // dischargerd issues discharges for all caveats present in the current
 // namespace with no additional caveats iff the caveat is valid.
 type dischargerd struct{}
@@ -26,7 +28,7 @@ func (dischargerd) Discharge(ctx *context.T, call rpc.ServerCall, caveat securit
 	if err := tp.Dischargeable(ctx, call.Security()); err != nil {
 		return security.Discharge{}, fmt.Errorf("third-party caveat %v cannot be discharged for this context: %v", tp, err)
 	}
-	expiry, err := security.NewExpiryCaveat(time.Now().Add(15 * time.Minute))
+	expiry, err := security.NewExpiryCaveat(time.Now().Add(dischargeExpiryTime))
 	if err != nil {
 		return security.Discharge{}, fmt.Errorf("unable to create expiration caveat on the discharge: %v", err)
 	}
@@ -36,7 +38,7 @@ func (dischargerd) Discharge(ctx *context.T, call rpc.ServerCall, caveat securit
 // NewDischarger returns a discharger service implementation that grants
 // discharges using the MintDischarge on the principal receiving the RPC.
 //
-// Discharges are valid for 15 minutes.
+// Discharges are valid for 24 hours.
 func NewDischarger() discharger.DischargerServerMethods {
 	return dischargerd{}
 }
