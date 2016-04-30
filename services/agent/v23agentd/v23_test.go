b79021391bd6c5464724bc6139849ab3762b7431
Bogdan Caprita <caprita@google.com>
2016-04-29 17:17:01 -0700

    services/agent/agentd: remove agentd
    Most of the test cases in agent_v23_test.go have been (or are in the
    process of) being migrated to other integration tests.  The last
    remaining one is the pingpong test, which is hereby moved to
    v23agentd/v23_test.go.
    
    Change-Id: I27ad69732e0d56a6143e52c7e9fc94bfbf20d5fd

--- services/agent/v23agentd/v23_test.go
+++ services/agent/v23agentd/v23_test.go
@@ -6,13 +6,19 @@ package main_test
 
 import (
 	"bufio"
+	"bytes"
+	"html/template"
+	"io/ioutil"
 	"os"
+	"path/filepath"
 	"strconv"
 	"strings"
+	"syscall"
 	"testing"
 
+	"v.io/v23/security"
 	"v.io/x/ref"
-	"v.io/x/ref/lib/security"
+	vsecurity "v.io/x/ref/lib/security"
 	_ "v.io/x/ref/runtime/factories/generic"
 	"v.io/x/ref/services/agent/internal/constants"
 	"v.io/x/ref/test/v23test"
@@ -54,11 +60,11 @@ func TestV23UnencryptedPrincipal(t *testing.T) {
 	testbin := v23test.BuildGoPkg(sh, "v.io/x/ref/services/agent/internal/test_principal")
 
 	credsDir := sh.MakeTempDir()
-	principal, err := security.CreatePersistentPrincipal(credsDir, nil)
+	principal, err := vsecurity.CreatePersistentPrincipal(credsDir, nil)
 	if err != nil {
 		t.Fatal(err)
 	}
-	if err := security.InitDefaultBlessings(principal, "happy"); err != nil {
+	if err := vsecurity.InitDefaultBlessings(principal, "happy"); err != nil {
 		t.Fatal(err)
 	}
 
@@ -79,11 +85,11 @@ func TestV23EncryptedPrincipal(t *testing.T) {
 	testbin := v23test.BuildGoPkg(sh, "v.io/x/ref/services/agent/internal/test_principal")
 
 	credsDir := sh.MakeTempDir()
-	principal, err := security.CreatePersistentPrincipal(credsDir, []byte("PASSWORD"))
+	principal, err := vsecurity.CreatePersistentPrincipal(credsDir, []byte("PASSWORD"))
 	if err != nil {
 		t.Fatal(err)
 	}
-	if err := security.InitDefaultBlessings(principal, "sneezy"); err != nil {
+	if err := vsecurity.InitDefaultBlessings(principal, "sneezy"); err != nil {
 		t.Fatal(err)
 	}
 
@@ -124,11 +130,11 @@ func TestV23Daemon(t *testing.T) {
 	testbin := v23test.BuildGoPkg(sh, "v.io/x/ref/services/agent/internal/test_principal")
 
 	credsDir := sh.MakeTempDir()
-	principal, err := security.CreatePersistentPrincipal(credsDir, []byte("P@SsW0rd"))
+	principal, err := vsecurity.CreatePersistentPrincipal(credsDir, []byte("P@SsW0rd"))
 	if err != nil {
 		t.Fatal(err)
 	}
-	if err := security.InitDefaultBlessings(principal, "grumpy"); err != nil {
+	if err := vsecurity.InitDefaultBlessings(principal, "grumpy"); err != nil {
 		t.Fatal(err)
 	}
 
@@ -141,4 +147,127 @@ func TestV23Daemon(t *testing.T) {
 	// Since we started the agent as a daemon, we need to explicitly stop it
 	// (since the Shell won't do it for us).
 	downComesAgentd(t, sh, credsDir)
+}
+
+func TestV23PingPong(t *testing.T) {
+	v23test.SkipUnlessRunningIntegrationTests(t)
+	sh := v23test.NewShell(t, nil)
+	defer sh.Cleanup()
+
+	// Setup two principals: One for the agent that runs the pingpong
+	// server, one for the client.  Since the server uses the default
+	// authorization policy, the client must have a blessing delegated from
+	// the server.
+	var (
+		clientDir, serverDir = createClientAndServerCredentials(t, sh)
+		pingpong             = v23test.BuildGoPkg(sh, "v.io/x/ref/services/agent/internal/pingpong")
+	)
+
+	upComesAgentd(t, sh, serverDir, "")
+	server := sh.Cmd(pingpong)
+	server.Vars[ref.EnvAgentPath] = constants.SocketPath(serverDir)
+	server.Start()
+	serverName := server.S.ExpectVar("NAME")
+
+	// Run the client via an agent once.
+	upComesAgentd(t, sh, clientDir, "")
+	client := sh.Cmd(pingpong, serverName)
+	client.Vars[ref.EnvAgentPath] = constants.SocketPath(clientDir)
+	client.Start()
+	client.S.Expect("Pinging...")
+	client.S.Expect("pong (client:[pingpongd:client] server:[pingpongd])")
+	client.Wait()
+
+	// Run it through a shell to test that the agent can pass credentials
+	// to subprocess of a shell (making things like "agentd bash" provide
+	// useful terminals).
+	// This only works with shells that propagate open file descriptors to
+	// children. POSIX-compliant shells do this as to many other commonly
+	// used ones like bash.
+	script := filepath.Join(sh.MakeTempDir(), "test.sh")
+	if err := writeScript(
+		script,
+		`#!/bin/bash
+echo "Running client"
+{{.Bin}} {{.Server}} || exit 101
+echo "Running client again"
+{{.Bin}} {{.Server}} || exit 102
+`,
+		struct{ Bin, Server string }{pingpong, serverName}); err != nil {
+		t.Fatal(err)
+	}
+	client = sh.Cmd("bash", script)
+	client.Vars[ref.EnvAgentPath] = constants.SocketPath(clientDir)
+	client.Start()
+	client.S.Expect("Running client")
+	client.S.Expect("Pinging...")
+	client.S.Expect("pong (client:[pingpongd:client] server:[pingpongd])")
+	client.S.Expect("Running client again")
+	client.S.Expect("Pinging...")
+	client.S.Expect("pong (client:[pingpongd:client] server:[pingpongd])")
+	client.Wait()
+	downComesAgentd(t, sh, clientDir)
+	server.Terminate(syscall.SIGINT)
+	downComesAgentd(t, sh, serverDir)
+}
+
+func writeScript(dstfile, tmpl string, args interface{}) error {
+	t, err := template.New(dstfile).Parse(tmpl)
+	if err != nil {
+		return err
+	}
+	var buf bytes.Buffer
+	if err := t.Execute(&buf, args); err != nil {
+		return err
+	}
+	if err := ioutil.WriteFile(dstfile, buf.Bytes(), 0700); err != nil {
+		return err
+	}
+	return nil
+}
+
+// createClientAndServerCredentials creates two principals, sets up their
+// blessings and returns the created credentials directories.
+//
+// The server will have a single blessing "pingpongd".
+// The client will have a single blessing "pingpongd:client", blessed by the
+// server.
+func createClientAndServerCredentials(t *testing.T, sh *v23test.Shell) (clientDir, serverDir string) {
+	clientDir = sh.MakeTempDir()
+	serverDir = sh.MakeTempDir()
+
+	pserver, err := vsecurity.CreatePersistentPrincipal(serverDir, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	pclient, err := vsecurity.CreatePersistentPrincipal(clientDir, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	// Server will only serve, not make any client requests, so only needs a
+	// default blessing.
+	bserver, err := pserver.BlessSelf("pingpongd")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if err := pserver.BlessingStore().SetDefault(bserver); err != nil {
+		t.Fatal(err)
+	}
+	// Clients need not have a default blessing as they will only make a request
+	// to the server.
+	bclient, err := pserver.Bless(pclient.PublicKey(), bserver, "client", security.UnconstrainedUse())
+	if err != nil {
+		t.Fatal(err)
+	}
+	if _, err := pclient.BlessingStore().Set(bclient, "pingpongd"); err != nil {
+		t.Fatal(err)
+	}
+	// The client and server must both recognize bserver and its delegates.
+	if err := security.AddToRoots(pserver, bserver); err != nil {
+		t.Fatal(err)
+	}
+	if err := security.AddToRoots(pclient, bserver); err != nil {
+		t.Fatal(err)
+	}
+	return
 }
